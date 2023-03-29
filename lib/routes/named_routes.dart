547e0c010289b14fb8090ea20efba3c329bb987a
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-03-29 00:28:26 -0300

    feat: adds new routes

--- lib/routes/named_routes.dart
+++ lib/routes/named_routes.dart
@@ -1,5 +1,7 @@
 class NamedRoutes {
   static const splash = 'splash_screen';
-  static const chat = 'chat_screen';
+  static const business = 'business_chat_screen';
+  static const videgame = 'videogame_chat_screen';
+  static const sports = 'sports_chat_screen';
   static const home = 'home_screen';
 }
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,7 +1,9 @@
+import 'package:chat_challenge/screens/chat_screens/sports_chat_screen.dart';
+import 'package:chat_challenge/screens/chat_screens/videogame_chat_screen.dart';
 import 'package:flutter/material.dart';
 
-import '../screens/home_screen.dart';
-import '../screens/chat_sceen.dart';
+import '../screens/chat_screens/business_chat_sceen.dart';
+import '../screens/home_screen/home_screen.dart';
 import '../screens/splash_screen.dart';
 import 'named_routes.dart';
 
@@ -10,7 +12,9 @@ class Routes {
     return {
       NamedRoutes.splash: (context) => const SplashScreen(),
       NamedRoutes.home: (context) => const HomeScreen(),
-      NamedRoutes.chat: (context) => const ChatScreen(),
+      NamedRoutes.business: (context) => const BusinessChatScreen(),
+      NamedRoutes.videgame: (context) => const VideoGameChatScreen(),
+      NamedRoutes.sports: (context) => const SportsChatScreen(),
     };
   }
 }
