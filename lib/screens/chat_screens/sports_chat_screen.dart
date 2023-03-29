1645a390acff66a8685a6cbf87342f269ece40a1
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-03-29 00:29:48 -0300

    feat: delete old ChatScreen & creates new ChatScreens

---
+++ lib/screens/chat_screens/sports_chat_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SportsChatScreen extends StatefulWidget {
+  const SportsChatScreen({super.key});
+
+  @override
+  State<SportsChatScreen> createState() => _SportsChatScreenState();
+}
+
+class _SportsChatScreenState extends State<SportsChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/screens/chat_screens/videogame_chat_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VideoGameChatScreen extends StatefulWidget {
+  const VideoGameChatScreen({super.key});
+
+  @override
+  State<VideoGameChatScreen> createState() => _VideoGameChatScreenState();
+}
+
+class _VideoGameChatScreenState extends State<VideoGameChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
