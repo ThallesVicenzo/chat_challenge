9171944351df5bcb977b53ab5fa566d1b00b7b4f
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-03-29 00:28:00 -0300

    feat: adds HomeScreen UI & functions

---
+++ lib/screens/home_screen/home_screen.dart
@@ -0,0 +1,62 @@
+import 'package:chat_challenge/routes/named_routes.dart';
+import 'package:chat_challenge/screens/home_screen/widgets/select_group_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        await SystemNavigator.pop();
+        return true;
+      },
+      child: Scaffold(
+        backgroundColor: Theme.of(context).primaryColor,
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: Center(
+            child: Text('Welcome !',
+                style: Theme.of(context).textTheme.bodyMedium),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text('Please choose your Group Chat',
+                  style: Theme.of(context).textTheme.bodyMedium),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SelectGroupButton(
+                    about: 'Business',
+                    icon: Icons.business,
+                    routes: NamedRoutes.business,
+                  ),
+                  SelectGroupButton(
+                    about: 'Videogame',
+                    icon: Icons.videogame_asset,
+                    routes: NamedRoutes.videgame,
+                  ),
+                  SelectGroupButton(
+                    about: 'Sports',
+                    icon: Icons.sports,
+                    routes: NamedRoutes.sports,
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
