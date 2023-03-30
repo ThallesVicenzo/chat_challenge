0011fc04fbef27a37d2eb192b566c28a2575dcd6
Thalles Vicenzo Costa <thalles.vicenzo.costa@gmail.com>
2023-03-30 01:58:25 -0300

    refactor: change functions to static functions

--- lib/services/shared_preferences.dart
+++ lib/services/shared_preferences.dart
@@ -1,15 +1,14 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class DataService {
-  void saveData(String nickname) async {
+  static void saveData(String nickname) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.setString('nickname', nickname);
   }
 
-  Future<String?> retrieveData(String nicknameKey) async {
+  static Future<String?> retrieveData(String nicknameKey) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String? nickname = prefs.getString(nicknameKey);
-    print(nickname);
 
     return nickname;
   }
